0d0d18f4b18106c785a4ea45bb5b64878c0abcc0
mike.chen <tmal0909@gmail.com>
2019-07-13 19:19:23 +0800

    後台管理 - FileManagementService - 暫存

---
+++ XinRevolution.Web/Services/Management/FileManagementService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XinRevolution.Web.Services.Management
+{
+    public class FileManagementService
+    {
+        public bool Save(string path, IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- XinRevolution.Web/Startup.cs
+++ XinRevolution.Web/Startup.cs
@@ -58,6 +58,7 @@ namespace XinRevolution.Web
             // Service
             services.AddScoped<UserMnagementService>();
             services.AddScoped<TagManagementService>();
+            services.AddScoped<FileManagementService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
