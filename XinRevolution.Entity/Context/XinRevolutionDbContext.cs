203fcd27b5d7861413b8c255d1ffa828025bbda3
mike <tmal0909@gmail.com>
2019-07-14 21:32:20 +0800

    Nuget 套件整合 & Seed Data 新增

--- XinRevolution.Entity/Context/XinRevolutionDbContext.cs
+++ XinRevolution.Entity/Context/XinRevolutionDbContext.cs
@@ -15,6 +15,16 @@ namespace XinRevolution.Entity.Context
             // 定義 Primary Key
             modelBuilder.Entity<UserModel>().HasKey(x => new { x.Account });
             modelBuilder.Entity<TagModel>().HasKey(x => new { x.TagName });
+
+            // 定義 Seed Data
+            modelBuilder.Entity<UserModel>().HasData(new UserModel {
+                Account = "mike.chen",
+                Password = "12345678",
+                Name = "陳彥翔",
+                Phone = "0916956546",
+                EMail = "tmal0909@gmail.com",
+                Address = "尚未編輯"
+            });
         }
 
         public DbSet<UserModel> Users { get; set; }
---
+++ XinRevolution.Entity/Migrations/20190714124326_UserAndTag.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace XinRevolution.Entity.Migrations
+{
+    public partial class UserAndTag : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    TagName = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Status = table.Column<bool>(type: "BIT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.TagName);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Account = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    EMail = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Account);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Account", "Address", "EMail", "Id", "Name", "Password", "Phone" },
+                values: new object[] { "mike.chen", "尚未編輯", "tmal0909@gmail.com", 0L, "陳彥翔", "12345678", "0916956546" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tags");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
