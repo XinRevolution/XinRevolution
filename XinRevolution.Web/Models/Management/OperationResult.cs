5110214f9bac6b36338f00b7c48d192d9e21ee62
mike <tmal0909@gmail.com>
2019-08-17 13:21:27 +0800

    暫存

---
+++ XinRevolution.Web/Models/Management/OperationResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XinRevolution.Web.Models.Management
+{
+    public class OperationResult<T>
+    {
+        public bool Status { get; set; }
+
+        public string Message { get; set; }
+
+        public T Data { get; set; }
+    }
+}
