bbde9fc278d880d3f5dc77f2991cc01df1a4f637
mike <tmal0909@gmail.com>
2019-08-24 17:21:28 +0800

    temp

--- XinRevolution.Manager/Controllers/HomeController.cs
+++ XinRevolution.Manager/Controllers/HomeController.cs
@@ -10,20 +10,9 @@ namespace XinRevolution.Manager.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Login()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- XinRevolution.Web/Startup.cs
+++ XinRevolution.Web/Startup.cs
@@ -13,7 +13,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using XinRevolution.Entity.Context;
 using XinRevolution.Entity.Model;
-using XinRevolution.Web.Services.Management;
 
 namespace XinRevolution.Web
 {
@@ -42,14 +41,11 @@ namespace XinRevolution.Web
                 options.AreaViewLocationFormats.Add("/Views/Common/{0}.cshtml");
             });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddDbContext<XinRevolutionDbContext>(options =>
             {
                 options.UseSqlServer(Configuration.GetConnectionString("Database"));
             });
-
-            // Management Service
-            services.AddScoped<BlobManagementService>();
         }
 
         public void Configure(IApplicationBuilder app, XinRevolutionDbContext dbContext)
